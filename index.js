bd55d02876e7e5f05142e85df6e56ac8fa9df845
comp500 <comp500@users.noreply.github.com>
2018-01-04 23:51:55 +0000

    Fix stuff

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const WebSocket = require("ws");
 const net = require("net");
  
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 9000 });
  
 wss.on("connection", (ws) => {
 	const tcpConn = net.createConnection({ port: 8999 }, () => {
 		console.log("Connected");
 	});
 	ws.on("message", (message) => {
-		console.log("received: %s", message);
-		tcpConn.write(message + "\r\n");
+		console.log("received: %s", message.toString());
+		tcpConn.write(message.toString() + "\r\n");
 	});
 	tcpConn.on("data", (data) => {
 		console.log("received: %s", data.toString());
