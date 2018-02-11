9932ec58ef2bd16c98aa496dabd5329fa9c53a36
comp500 <comp500@users.noreply.github.com>
2018-02-11 22:58:45 +0000

    Remove dummy code

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const wss = new WebSocket.Server({ port: 9000 });
 wss.on("connection", (ws) => {
 	const tcpConn = net.createConnection({ port: 8999 }, () => {
 		console.log("Connected");
-		tcpConn.write(`{"Hello": {"username": "Bob", "room": {"name": "SyncRoom"}, "version": "1.2.7"}}\r\n`);
-		var latency = Math.floor(new Date() / 1000);
-		tcpConn.write(`{"State": {"ping": {"clientRtt": 0, "clientLatencyCalculation": ${latency}, "latencyCalculation": ${latency}}, "playstate": {"paused": true, "position": 0.0}}}\r\n`);
-		setTimeout(function () {
-			var latency = Math.floor(new Date() / 1000);
-			tcpConn.write(`{"State": {"ping": {"clientRtt": 0, "clientLatencyCalculation": ${latency}, "latencyCalculation": ${latency}}, "playstate": {"paused": true, "position": 0.0}}}\r\n`);
-		}, 1000);
+		//tcpConn.write(`{"Hello": {"username": "Bob", "room": {"name": "SyncRoom"}, "version": "1.2.7"}}\r\n`);
+		//var latency = Math.floor(new Date() / 1000);
+		//tcpConn.write(`{"State": {"ping": {"clientRtt": 0, "clientLatencyCalculation": ${latency}, "latencyCalculation": ${latency}}, "playstate": {"paused": true, "position": 0.0}}}\r\n`);
+		//setTimeout(function () {
+		//	var latency = Math.floor(new Date() / 1000);
+		//	tcpConn.write(`{"State": {"ping": {"clientRtt": 0, "clientLatencyCalculation": ${latency}, "latencyCalculation": ${latency}}, "playstate": {"paused": true, "position": 0.0}}}\r\n`);
+		//}, 1000);
 	});
 	ws.on("message", (message) => {
 		console.log("received: %s", message.toString());
